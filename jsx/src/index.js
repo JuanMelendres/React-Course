fe8786d82816784de8ef5002896f88a8d8cd85ae
JuanMelendres <juan.mele97@gmail.com>
2021-06-11 12:55:16 -0500

    Update jsx app
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,10 +1,13 @@
-// Import the React and ReactDOM libraries
+// Import the react and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a react component
+// Crate a react component
 const App = () => {
-    return <div> Hi there!</div>;
+  return <div>Hi there!</div>;
 };
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
