6bacf90a4e7b9b1ab35b985936e89a98c6baa712
JuanMelendres <juan.mele97@gmail.com>
2021-06-21 13:16:52 -0500

    Add content with JSX
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// function getButtonText() {
+//   return 'Click on me!';
+// getButtonText()
+// }
+
 // Crate a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: 'Click Me!' };
+  const style = { backgroundColor: 'blue', color: 'white' };
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <label className="label" htmlFor="name">
+        { labelText }
+      </label>
+      <input id="name" type="text"></input>
+      <button style={ style }>
+        { buttonText.text }
+      </button>
+    </div>
+  );
 };
 // Take the react component and show it on the screen
 ReactDOM.render(
