584a4636297c54179e8f6cd616e0292880421c9e
JuanMelendres <juan.mele97@gmail.com>
2021-06-21 14:44:27 -0500

    Add components
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ components/src/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={ faker.image.avatar() }/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        { faker.internet.userName() }
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at { faker.date.recent().toLocaleTimeString() }</span>
+                    </div>
+                    <div className="text">{faker.lorem.paragraph()}</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={ faker.image.avatar() }/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        { faker.internet.userName() }
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at { faker.date.recent().toLocaleTimeString() }</span>
+                    </div>
+                    <div className="text">{ faker.lorem.paragraph() }</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={ faker.image.avatar() }/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        { faker.internet.userName() }
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at { faker.date.recent().toLocaleTimeString() }</span>
+                    </div>
+                    <div className="text">{ faker.lorem.paragraph() }</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
