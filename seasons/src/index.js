fa691d7194a56c7a7463c5eafea7edd93d427f00
JuanMelendres <juan.mele97@gmail.com>
2021-06-24 12:34:09 -0500

    Add Spinner component
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     // constructor(props) {
@@ -30,29 +31,20 @@ class App extends React.Component {
             );
         } 
         if (!this.state.errorMessage && this.state.lat) {
-            return (
-                <div className="ui">
-                    <SeasonDisplay lat={this.state.lat} />
-                </div>
-            );
+            return <SeasonDisplay lat={this.state.lat} />;
         }
-        return (
-            <div className="ui">
-                <div className="ui active dimmer">
-                    <div className="ui text loader">Loading</div>
-                </div>
-            </div>
-        );
+        return <Spinner/>;
     }
-    // <div className="ui">
-    //      <h1>Loading !!!</h1>
-    // </div>
 }
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+
+    // <div className="ui">
+    //      <h1>Loading !!!</h1>
+    // </div>
 
     // Good place to do data-loading!!
     // componentDidMount() {
